9be96b7133da3ee63ed51ba698cff68f5466a0b0
Patryk Poborca <poborcapatryk@gmail.com>
2016-07-10 12:56:26 +0200

    okay, provider is not being generated

--- app/src/main/java/patrykpoborca/io/plagrounds/MainActivity.java
+++ app/src/main/java/patrykpoborca/io/plagrounds/MainActivity.java
@@ -3,12 +3,15 @@ package patrykpoborca.io.plagrounds;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
-    
+import patrykpoborca.io.othermodule.BaseActivity;
+
+public class MainActivity extends BaseActivity<SubclassOfBaseClassTwo> {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
